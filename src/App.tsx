c029e2808fec70da9cb31b2a0b04a4c8f7ea34a1
Walid VM <walid.hanifi707@gmail.com>
2022-12-24 20:57:03 +1000

    Add structure for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './styles/style.css'
 
-function App() {
+import Footer from './components/footer'
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className="book-container">
+
+        <div id="cover">
+          <div className="effect"></div>
+          <img src="https://www.booktopia.com.au/blog/wp-content/uploads/2010/01/9781406305890.jpg?w=160"></img>
+        </div>
+
+        <div id="contents">
+          <div id="header">
+            <h3>A <span className="blue">WHERE'S</span> <span className="red">WALLY</span> GAME made by... Wally!</h3>
+          </div>
+          <form method="POST" action=''>
+            <label id="username">
+              Username:
+              <input name='username' type="text" placeholder="Username"></input>
+            </label>
+            <label id="checkbox">
+              Leaderboard?
+              <input name='checkbox' type="checkbox"></input>
+            </label>
+            <label id="difficulty">
+              Difficulty:
+              <select name="platform">
+                <option>Level 1 (Easy)</option>
+                <option>Level 1 (Medium)</option>
+                <option>Level 3 (Hard)</option>
+              </select>
+            </label>
+            <input type="submit" value="START GAME" className="start-btn"></input>
+          </form>
+        </div>
+
     </div>
-  );
+
+    < Footer />
+
+    </div>
+  )
 }
 
-export default App;
+export default App
