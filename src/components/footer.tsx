033f3e281b7b21905cbb24e6d2eb3b9556c086db
Walid VM <walid.hanifi707@gmail.com>
2022-12-24 20:54:56 +1000

    Initial commit

---
+++ src/components/footer.tsx
@@ -0,0 +1,12 @@
+const footer = () => {
+    return (
+        <div id="footer">
+            <span id='footer-title'>Copyright © 2022 Waldorfio&nbsp;&nbsp;&nbsp;
+                <a href="https://www.github.com/waldorfio" id='github-logo' className="fa fa-github"></a>
+                <a href="https://www.linkedin.com/in/wally-h/" id='linked-logo' className="fa fa-linkedin"></a>
+            </span>
+        </div>
+    )
+}
+
+export default footer
